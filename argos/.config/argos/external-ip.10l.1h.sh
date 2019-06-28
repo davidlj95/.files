684c621704e4cf6c115bac547a875fe4c9f33073
David LJ <mail@davidlj95.com>
2019-06-28 04:38:11 +0200

    Added lots of argos extensions

---
+++ argos/.config/argos/external-ip.10l.1h.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# <bitbar.title>external-ip</bitbar.title>
+# <bitbar.author>Mat Ryer</bitbar.author>
+# <bitbar.author.github>matryer</bitbar.author.github>
+# <bitbar.desc>Gets the current external IP address.</bitbar.desc>
+EXTERNAL_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
+if [ "$1" = "copy" ]; then
+  # Copy the IP to clipboard
+  echo -n "$EXTERNAL_IP" | pbcopy
+fi
+
+echo ":globe_with_meridians:  $EXTERNAL_IP"
+echo "---"
+echo "(External IP address)"
+echo "Copy IP | terminal=false bash='$0' param1=copy"
