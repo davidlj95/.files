9b038047b9d76f31c8f2c27de72f41ef56125392
David LJ <mail@davidlj95.com>
2019-07-02 18:07:49 +0200

    argos extensions: update on dropdown

--- argos/.config/argos/top.3s+.sh
+++ argos/.config/argos/top.3s+.sh
@@ -5,7 +5,7 @@ echo "---"
 
 if [ "$ARGOS_MENU_OPEN" == "true" ]; then
   # http://stackoverflow.com/a/14853319
-  TOP_OUTPUT=$(top -b -n 1 | head -n 20 | awk 1 ORS="\\\\n")
+  TOP_OUTPUT=$(top -Em -b -n 1 | head -n 20 | awk 1 ORS="\\\\n")
   echo "$TOP_OUTPUT | font=monospace bash=top"
 else
   echo "Loading..."
